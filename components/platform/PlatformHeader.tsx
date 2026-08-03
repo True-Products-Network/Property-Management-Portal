1cb927f6502a97e41705a5c679bbd2ac6301af92
Nigel Lear <nigel@trueproductsnetwork.com>
2026-08-03 15:04:01 +0000

    Remove forceMount prop from DropdownMenuContent

--- components/platform/PlatformHeader.tsx
+++ components/platform/PlatformHeader.tsx
@@ -60,7 +60,7 @@ export function PlatformHeader({ user, role }: PlatformHeaderProps) {
                 </div>
               </Button>
             </DropdownMenuTrigger>
-            <DropdownMenuContent className="w-56" align="end" forceMount>
+            <DropdownMenuContent className="w-56" align="end">
               <DropdownMenuLabel className="font-normal">
                 <div className="flex flex-col space-y-1">
                   <p className="text-sm font-medium">{user.email}</p>
