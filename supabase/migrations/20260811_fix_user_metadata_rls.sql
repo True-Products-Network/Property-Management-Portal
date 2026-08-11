413437c0397a8d6a1c1419c6f8a9795263f4e6e7
Nigel Lear <nigel@trueproductsnetwork.com>
2026-08-11 12:42:28 +0000

    fix: Cast slug to uuid for comparison in get_user_business_ids

--- supabase/migrations/20260811_fix_user_metadata_rls.sql
+++ supabase/migrations/20260811_fix_user_metadata_rls.sql
@@ -17,7 +17,7 @@ STABLE
 SECURITY DEFINER
 AS $$
   SELECT b.id::text FROM businesses b
-  WHERE b.slug IN (SELECT get_user_tenant_ids());
+  WHERE b.slug::uuid IN (SELECT get_user_tenant_ids());
 $$;
 
 -- Compliance Matters - secure policy
